83e3a9821a5ea6236cee90d44914af5ef1655fc3
Arjun Pai <your-email@example.com>
2025-12-12 08:51:20 +0530

    cxz

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -5,6 +5,7 @@ const api = axios.create({
     import.meta.env.VITE_API_URL ||
     // "http://localhost:3000" ||
     "https://blogify-4kr7.onrender.com/", // adjust to your backend
+  withCredentials: true,
 });
 
 export default api;
