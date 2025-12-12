f4f78d96740ddf856bd5010df7f4f42ea9ee135d
Arjun Pai <your-email@example.com>
2025-12-12 08:31:51 +0530

    updated base url

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000", // adjust to your backend
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    // "http://localhost:3000" ||
+    "https://blogify-4kr7.onrender.com/", // adjust to your backend
 });
 
 export default api;
