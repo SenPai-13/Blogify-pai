11b7193d36c0fad02968dfdcf5972d596a548c0f
Arjun Pai <your-email@example.com>
2025-12-15 13:07:11 +0530

    .

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { getMe, login, sendOtp, verifyOtp } from "../controllers/authController";
+import { getMe, login} from "../controllers/authController";
 import { protect } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtp);
+// router.post("/send-otp", sendOtp);
+// router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 
 router.get("/me", protect, getMe);
